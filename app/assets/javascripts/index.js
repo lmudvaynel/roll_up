9c5a69c1c1c23e5be3c6943bdbaa35afd9dd98a4
Artem D <artdemko@gmail.com>
2014-02-28 10:28:38 +0300

    Pretty photo

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,22 +1,13 @@
 $(document).ready(function(){
 
-	var fullHeight = $(window).height();
-	if(fullHeight > 980) { $('.wrapper').css({'margin-top':(fullHeight-980)/2+'px'}); }
-	else { $('.wrapper').css({'margin-top':'0px'});}
-	$(window).resize(function() {
-	var fullHeight = $(window).height();
-		if(fullHeight > 980) { $('.wrapper').css({'margin-top':(fullHeight-980)/2+'px'}); }
-		else { $('.wrapper').css({'margin-top':'0px'});}
-	});
-
 	$(".gallery_1 a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook', /* light_rounded / dark_rounded / light_square / dark_square / facebook */slideshow:5000, autoplay_slideshow:true});
-	$(".gallery_2 a[rel^='prettyPhoto']").prettyPhoto({slideshow:5000, autoplay_slideshow:true});
-	$(".gallery_3 a[rel^='prettyPhoto']").prettyPhoto({slideshow:5000, autoplay_slideshow:true});
-	$(".gallery_4 a[rel^='prettyPhoto']").prettyPhoto({slideshow:5000, autoplay_slideshow:true});
-
-
+	$(".gallery_2 a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook', /* light_rounded / dark_rounded / light_square / dark_square / facebook */slideshow:5000, autoplay_slideshow:true});
+	$(".gallery_3 a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook', /* light_rounded / dark_rounded / light_square / dark_square / facebook */slideshow:5000, autoplay_slideshow:true});
+	$(".gallery_4 a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook', /* light_rounded / dark_rounded / light_square / dark_square / facebook */slideshow:5000, autoplay_slideshow:true});
+	$(".gallery_5 a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook', /* light_rounded / dark_rounded / light_square / dark_square / facebook */slideshow:5000, autoplay_slideshow:true});
 
 	$(".column").mouseenter(function(){
+		$(this).find(".more").removeClass('hidden')
 		$(this).addClass("current");
 		$(this).find(".stand_name").addClass("current_name")
 		$(this).stop().animate({
@@ -28,6 +19,7 @@ $(document).ready(function(){
 			
 	}).mouseleave(function(){
 		$(this).removeClass("current")
+		$(this).find(".more").addClass('hidden')
 		$(this).stop().animate({
 		},500);
 		if ($(this).hasClass("no_border") ) {
