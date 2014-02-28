ff42720d641a90d02a8650c7776327ac98ecd470
Artem D <artdemko@gmail.com>
2014-02-28 15:20:53 +0300

    Mailer update

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,10 @@
+# encoding: utf-8
 class OrderMailer < ActionMailer::Base
   default from: "noreply@me31.ru"
 
   def order_mail(order)
   	@order = order
     mail :to => AdminUser.all.map(&:email),
-         :subject => "Vizov"
+         :subject => "Заказ стенда"
   end
 end
