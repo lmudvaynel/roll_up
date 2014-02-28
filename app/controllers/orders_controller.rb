a06a5c602aac7149d9b40af9c525a5e08a075ea6
Artem D <artdemko@gmail.com>
2014-02-28 15:37:28 +0300

    Flash success order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
+# encoding: utf-8
 class OrdersController < ApplicationController
-
   def create
-      @order = Order.create(params[:order])
-      if @order.save
-          OrderMailer.order_mail(@order).deliver
-        redirect_to root_url
-      else
-        render 'pages/order'
-      end
+    @order = Order.create(params[:order])
+    if @order.save
+      OrderMailer.order_mail(@order).deliver
+      flash[:success] = "Спасибо! Ваш заказ оформлен!"
+      redirect_to root_url
+    else
+      render 'pages/order'
     end
+  end
 end
