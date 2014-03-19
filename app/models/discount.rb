2a83db3a0de8c7c5c883fd11c837b86f8e0ef747
Artem D <artdemko@gmail.com>
2014-03-19 12:38:54 +0300

    Discount without link

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,7 +3,7 @@ class Discount < ActiveRecord::Base
   
   has_attached_file :image,
  					:styles => { :medium => "600x600>", :thumb => "100x100>" }
-  
+
   validates_attachment_presence :image
   validates_attachment_content_type :image,:content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
   validates_attachment_size :image,:less_than => 5.megabytes
